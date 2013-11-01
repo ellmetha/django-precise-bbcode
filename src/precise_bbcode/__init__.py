e3411a3eb09873c001671d514792853a3fcbc605
ellmetha <morgan.aubert.fr@gmail.com>
2013-11-01 19:29:50 +0100

    __init__ filled

---
+++ src/precise_bbcode/__init__.py
@@ -0,0 +1,6 @@
+version_info = (0, 0, 6)
+
+__version__ = version = '.'.join(map(str, version_info ))
+__project__ = PROJECT = 'django-precise-bbcode'
+__author__ = AUTHOR = "Morgan Aubert <morgan.aubert@zoho.com>"
+__license__ = LICENSE = "BSD"
