b10b6637e4793d1d24fbf2f0a022b11cdfe707dc
ellmetha <morgan.aubert.fr@gmail.com>
2013-11-06 00:34:28 +0100

    setup updated

--- setup.py
+++ setup.py
@@ -1,19 +1,44 @@
-from setuptools import setup, find_packages
+from os.path import abspath
+from os.path import dirname
+from os.path import join
+from setuptools import find_packages
+from setuptools import setup
+
+
+def read_relative_file(filename):
+    """
+    Returns contents of the given file, whose path is supposed relative
+    to this module.
+    """
+    with open(join(dirname(abspath(__file__)), filename)) as f:
+        return f.read()
+
 
 setup(
     name='django-precise-bbcode',
-    version='0.1',
-    author='ellmetha',
-    author_email='ell@metha.fr',
+    version=read_relative_file('VERSION').strip(),
+    author='Morgan Aubert',
+    author_email='morgan.aubert@zoho.com',
     package_dir={'': 'src'},
     packages=find_packages("src", exclude="example_project"),
     include_package_data=True,
-    license='LICENSE.txt',
+    license='BSD license, see LICENSE file',
     description='A django BBCode integration..',
     long_description=open('README.rst').read(),
     zip_safe=False,
     install_requires=[
-        "django>=1.5,<1.6",
+        "django",
         "south",
+    ],
+    classifiers=[
+        'Development Status :: 1 - Alpha',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
     ],
 )
