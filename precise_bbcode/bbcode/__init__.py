af30ad672ef843aa9b40c5ae0ec82ec6bf3bd971
ellmetha <morgan.aubert@zoho.com>
2014-10-14 23:59:19 +0200

    Placeholder pool added

--- precise_bbcode/bbcode/__init__.py
+++ precise_bbcode/bbcode/__init__.py
@@ -42,6 +42,9 @@ class BBCodeParserLoader(object):
         # Init default BBCode placeholders
         self.init_default_bbcode_placeholders()
 
+        # Init placeholders registered in 'bbcode_placeholders' modules
+        self.init_bbcode_placeholders()
+
         # Init default BBCode tags
         self.init_default_bbcode_tags()
 
@@ -57,12 +60,29 @@ class BBCodeParserLoader(object):
             self.init_bbcode_smilies()
 
     def init_default_bbcode_placeholders(self):
+        """
+        Find the default placeholders and makes them available for the parser.
+        """
         import precise_bbcode.bbcode.defaults.placeholder
         for placeholder_klass in get_subclasses(
                 precise_bbcode.bbcode.defaults.placeholder, BBCodePlaceholder):
+            setattr(placeholder_klass, 'default_placeholder', True)
             self.parser.add_placeholder(placeholder_klass)
 
+    def init_bbcode_placeholders(self):
+        """
+        Call the BBCode placeholder pool to fetch all the module-based placeholders
+        and initializes them.
+        """
+        from precise_bbcode.placeholder_pool import placeholder_pool
+        placeholders = placeholder_pool.get_placeholders()
+        for placeholder in placeholders:
+            self.parser.add_placeholder(placeholder)
+
     def init_default_bbcode_tags(self):
+        """
+        Find the default bbcode tags and makes them available for the parser.
+        """
         import precise_bbcode.bbcode.defaults.tag
         for tag_klass in get_subclasses(
                 precise_bbcode.bbcode.defaults.tag, BBCodeTag):
---
+++ precise_bbcode/placeholder_pool.py
@@ -0,0 +1,75 @@
+# -*- coding: utf-8 -*-
+
+# Standard library imports
+
+# Third party imports
+from django.core.exceptions import ImproperlyConfigured
+
+# Local application / specific library imports
+from .bbcode.placeholder import BBCodePlaceholder
+from .core.loading import load
+
+
+class PlaceholderAlreadyRegistered(Exception):
+    pass
+
+
+class PlaceholderNotRegistered(Exception):
+    pass
+
+
+class PlaceholderPool(object):
+    """
+    BBCode placeholders are registered with the PlaceholderPool using the register() method. It makes them
+    available to the BBCode parser.
+    """
+
+    def __init__(self):
+        self.placeholders = {}
+        self.discovered = False
+
+    def discover_placeholders(self):
+        if self.discovered:
+            return
+        self.discovered = True
+        load('bbcode_placeholders')
+
+    def register_placeholder(self, placeholder):
+        """
+        Registers the given placeholder(s).
+        If a placeholder appears to be already registered, a PlaceholderAlreadyRegistered exception will be raised.
+        """
+        # A registered placeholder must be a subclass of BBCodePlaceholder
+        if not issubclass(placeholder, BBCodePlaceholder):
+            raise ImproperlyConfigured(
+                'BBCode Placeholders must be subclasses of BBCodePlaceholder, {!r} is not'.format(placeholder)
+            )
+
+        # Two placeholders with the same names can't be registered
+        placeholder_name = placeholder.name
+        if placeholder_name in self.placeholders:
+            raise PlaceholderAlreadyRegistered(
+                'Cannot register {!r}, a placeholder with this name ({!r}) is already registered'.format(
+                    placeholder, placeholder_name)
+            )
+
+        self.placeholders[placeholder_name] = placeholder
+
+    def unregister_placeholder(self, placeholder):
+        """
+        Unregister the given placeholder(s).
+        If a placeholder appears to be not registered, a PlaceholderNotRegistered exception will be raised.
+        """
+        placeholder_name = placeholder.name
+        if placeholder_name not in self.placeholders:
+            raise PlaceholderNotRegistered(
+                'The placeholder {!r} is not registered'.format(placeholder)
+            )
+        del self.placeholders[placeholder_name]
+
+    def get_placeholders(self):
+        self.discover_placeholders()
+        return self.placeholders.values()
+
+
+placeholder_pool = PlaceholderPool()
