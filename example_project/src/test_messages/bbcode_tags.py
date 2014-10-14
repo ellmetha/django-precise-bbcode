ae84db778b93915eed0596fb8cf34b281e6fae96
ellmetha <morgan.aubert@zoho.com>
2014-10-14 23:59:31 +0200

    Example project updated

--- example_project/src/test_messages/bbcode_tags.py
+++ example_project/src/test_messages/bbcode_tags.py
@@ -36,7 +36,26 @@ class FruitTag(BBCodeTag):
     format_string = '<h5>{CHOICE=tomato,orange,apple}</h5>'
 
 
+class PhoneLinkTag(BBCodeTag):
+    name = 'phone'
+    definition_string = '[phone]{PHONENUMBER}[/phone]'
+    format_string = '<a href="tel:{PHONENUMBER}">{PHONENUMBER}</a>'
+
+    def render(self, value, option=None, parent=None):
+        href = 'tel:{}'.format(value)
+        # Render
+        return '<a href="{0}">{0}</a>'.format(href, value)
+
+
+class StartsWithATag(BBCodeTag):
+    name = 'startswitha'
+    definition_string = '[startswitha]{STARTSWITH=a}[/startswitha]'
+    format_string = '<span>{STARTSWITH=a}</span>'
+
+
 tag_pool.register_tag(SubTag)
 tag_pool.register_tag(PreTag)
 tag_pool.register_tag(SizeTag)
 tag_pool.register_tag(FruitTag)
+tag_pool.register_tag(PhoneLinkTag)
+tag_pool.register_tag(StartsWithATag)
