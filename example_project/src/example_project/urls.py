c8bcea76cc9e4559c2f7f05844d906838cec8ca2
ellmetha <morgan.aubert@zoho.com>
2016-01-31 13:14:09 -0500

    Example project updated

--- example_project/src/example_project/urls.py
+++ example_project/src/example_project/urls.py
@@ -1,8 +1,8 @@
 from django.conf import settings
-from django.conf.urls import patterns, include, url
+from django.conf.urls import include, url
 from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-
+from django.views.static import serve
 from test_messages.views import TestMessageCreate
 from test_messages.views import TestMessageDetailView
 
@@ -11,20 +11,20 @@ from test_messages.views import TestMessageDetailView
 admin.autodiscover()
 
 # Patterns
-urlpatterns = patterns('',
+urlpatterns = [
     # Admin
     url(r'^' + settings.ADMIN_URL, include(admin.site.urls)),
 
     # Apps
-    (r'^$', TestMessageCreate.as_view()),
+    url(r'^$', TestMessageCreate.as_view()),
     url(r'^testmessage/(?P<message_pk>\d+)/$', TestMessageDetailView.as_view(), name="bbcode-message-detail"),
-)
+]
 
 # # In DEBUG mode, serve media files through Django.
 if settings.DEBUG:
     urlpatterns += staticfiles_urlpatterns()
     # Remove leading and trailing slashes so the regex matches.
     media_url = settings.MEDIA_URL.lstrip('/').rstrip('/')
-    urlpatterns += patterns('',
-        url(r'^%s/(?P<path>.*)$' % media_url, 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
-    )
+    urlpatterns += [
+        url(r'^%s/(?P<path>.*)$' % media_url, serve, {'document_root': settings.MEDIA_ROOT}),
+    ]
